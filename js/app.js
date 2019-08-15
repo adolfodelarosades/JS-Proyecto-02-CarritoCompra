dfda522dc803ef69fe53404ccdf1105f93f8b957
Adolfo <adolfodelarosa2012@gmail.com>
2019-08-15 12:15:03 +0200

    Recuperar datos del curso seleccionado y pintarlos en el Carrito de Compra

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
 //VARIABLES
 const carrito = document.getElementById('carrito');
 const cursos = document.getElementById('lista-cursos');
+const listaCursos = document.querySelector('#lista-carrito tbody');
 
 //FUNCIONES
-
-// Lee los datos del curso
-function leerDatosCurso(curso){
-    console.log(curso);
-}
 
 //Función que añade el curso al carrito
 function comprarCurso(e){
@@ -20,6 +16,36 @@ function comprarCurso(e){
         // Enviamos curso seleccionado para tomar sus datos
         leerDatosCurso(curso);
     }
+}
+
+// Lee los datos del curso
+function leerDatosCurso(curso){
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id')
+    }
+    console.log(curso);
+    console.log(infoCurso);
+
+    insertarCarrito(infoCurso);
+}
+
+//Muestra el curso seleccionado en el Carrito
+function insertarCarrito(curso){
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${curso.imagen}" width=100 >
+        </td>
+        <td>${curso.titulo}</td>
+        <td>${curso.precio}</td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+        </td>
+    `;
+    listaCursos.appendChild(row);
 }
 
 // Detecta todos los lisners
